72aeb35edf52e8c56c97713d66173a87fe58979b
Munif Gebara Junior <munif@MacBook-Pro-de-Munif.local>
2016-08-02 22:23:39 -0300

    Navegacão básica

--- www/js/app.js
+++ www/js/app.js
@@ -1,18 +1,34 @@
-var aplicativo=angular.module('starter', ['ionic']);
+var aplicativo=angular.module('encatman', ['ionic']);
 
-console.log("Aqui1");
+aplicativo.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider.state('list', {
+    url: '/list',
+    templateUrl: 'templates/list.html',
+    controller: 'ListaCtrl'
+  });
+
+  $stateProvider.state('votar', {
+    url: '/votar/:enqueteId',
+    templateUrl: 'templates/votar.html',
+    controller: 'VotaCtrl'
+  });
+
+  $urlRouterProvider.otherwise('/list');
+});
+
 aplicativo.controller('ListaCtrl', function($http, $scope) {
-
   $scope.enquetes=[{"nome":"valor"}];
   $http.get('https://munif.com.br/encatman-api/api/enquete?gumgaToken=fafiman')
   .success(function(response) {
     $scope.enquetes=response;
   });
-
-
-
-
 });
+
+aplicativo.controller('VotaCtrl', function($http, $scope) {
+  $scope.enquete=[{"titulo":"valor","texto":"alguma coisa"}];
+});
+
 
 
 aplicativo.run(function($ionicPlatform) {
