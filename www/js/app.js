f8299203e378dc77eca7d81e1d4e750f93d1f1fb
Munif Gebara Junior <munif@MacBook-Pro-de-Munif.local>
2016-08-22 20:26:40 -0300

    Votando

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,11 @@
 var aplicativo=angular.module('encatman', ['ionic']);
 
-aplicativo.config(function($stateProvider, $urlRouterProvider) {
+aplicativo.config(function($stateProvider, $urlRouterProvider,$httpProvider) {
+
+  $httpProvider.defaults.headers.common = {
+          'gumgaToken': 'fafiman'
+        };
+
 
   $stateProvider.state('list', {
     url: '/list',
@@ -27,14 +32,24 @@ aplicativo.controller('ListaCtrl', function($http, $scope) {
 
 aplicativo.controller('VotaCtrl', function($http, $scope, $state, $stateParams) {
   $scope.enquete={};
+  $scope.status="Não votou";
+  $scope.votou=false;
   id=$stateParams.enqueteId;
-  $http.get('https://munif.com.br/encatman-api/api/enquete/'+id+'?gumgaToken=fafiman')
+  $http.get('https://munif.com.br/encatman-api/api/enquete/'+id)
   .success(function(response) {
     $scope.enquete=response;
   });
-  $scope.responde=function(enqueteId,respostaId){
-    console.log(enqueteId,respostaId);
-    //http://munif.com.br/encatman-api/api/resposta
+  $scope.responde=function(resposta){
+    data={opcao:resposta};
+    $http.post('https://munif.com.br/encatman-api/api/resposta',data)
+    .then(function(response) {
+      $scope.status="Votou "+resposta.texto;
+      $scope.votou=true;
+    },
+    function(response) {
+      $scope.status="Problemas ao votar";
+    });
+
   }
 
 });
